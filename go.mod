8b5a8dd3ad5c2914d53931a088edb1cf10ab3b42
samrityy <smritymulmi23@gmail.com>
2025-07-23 01:09:50 +0545

    project initialization

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/samrityy/file-encryption
+
+go 1.24.3
---
+++ main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		printHelp()
+		os.Exit(0)
+	}
+	function := os.Args[1]
+
+	switch function {
+	case "help":
+		printHelp()
+	case "encrypt":
+		encryptHandle()
+	case "decrypt":
+		decryptHandle()
+	default:
+		fmt.Println("Run encrypt to encrypt a file and decrypt to decrypt  a file ")
+		os.Exit(1)
+	}
+}
+
+func printHelp() {
+	fmt.Println("file encryption")
+	fmt.Println("Simple file encryprion for your day-to-day needs")
+	fmt.Println("")
+	fmt.Println("Usage:")
+	fmt.Println("")
+	fmt.Println("\t go run . encrypt a file given a password")
+	fmt.Println("\t decrypt \t tries to decrypt a file using a password")
+	fmt.Println("\t help \t\t Display help text")
+	fmt.Println("")
+}
+func encryptHandle() {
+	if len(os.Args) < 3 {
+		println("missing teh path to the file .For more info, run the help command")
+		os.Exit(0)
+	}
+	file := os.Args[2]
+	if !validateFile(file) {
+		panic("File not found")
+	}
+	password := getPassword()
+	fmt.Println("\nEcrypting....")
+	filecrypt.Encrypt(file, password)
+	fmt.Println("File encrypted successfully")
+
+}
+func decryptHandle() {
+}
+func getPassword() []byte {
+	fmt.Print("Enter password: ")
+	password, _ := term.ReadPassword(0)
+	fmt.Print("/n Confirm Password")
+	password2, _ := term.ReadPassword(0)
+	if !validatePassword(password, password2) {
+		fmt.Print("\n Password doo not match , please try again\n")
+		return getPassword()
+	}
+}
+
+func validatePassword(password1 []byte, password2 []byte) bool {
+	if !bytes.Equal(password1, password2) {
+		return false
+	}
+	return true
+}
+
+func validateFile(file string) bool {
+	if _, err := os.Stat(file); os.IsNotExist(err) {
+		return false
+	}
+	return true
+}
